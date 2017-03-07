f3e094d205ae74966c05b019d5c3bb11f99599bc
Alex Mastro <amastro@apple.com>
2017-03-07 14:05:16 -0800

    Simplify GPIO class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdint>
+#include <type_traits>
 
 void escape(void* p)
 {
@@ -21,8 +22,9 @@ void write_bits(volatile uint32_t& dst, uint32_t mask, int pos, uint32_t bits)
 	dst = (dst & ~(mask << pos)) | ((mask & bits) << pos);
 }
 
-struct RCC
+class RCC
 {
+public:
 	static constexpr uintptr_t base = 0x40023800U;
 	volatile uint32_t& AHB1ENR = memory(base + 0x30);
 
@@ -30,14 +32,15 @@ struct RCC
 };
 
 enum class Port { A, B, C, D, E, F, G, H, I, J, K };
-enum class Speed { LOW, MEDIUM, HIGH, VERY_HIGH };
-enum class Pull { NONE, UP, DOWN };
-enum class OutputType { PUSH_PULL, OPEN_DRAIN };
-enum class Mode { INPUT, OUTPUT, ALTERNATE_FUNCTION, ANALOG };
+enum class Speed { Low, Medium, High, VeryHigh };
+enum class Pull { None, Up, Down };
+enum class OutputType { PushPull, OpenDrain };
+enum class Mode { Input, Output, AlternateFunction, Analog };
 
 template<Port P>
-struct GPIO
+class GPIO
 {
+public:
 	static constexpr uintptr_t base = 0x40020000U + (static_cast<int>(P) * 0x400U);
 	volatile uint32_t& MODER = memory(base + 0x00);
 	volatile uint32_t& OTYPER = memory(base + 0x04);
@@ -49,51 +52,12 @@ struct GPIO
 	volatile uint32_t& LCKR = memory(base + 0x1C);
 	volatile uint32_t& AFRL = memory(base + 0x20);
 	volatile uint32_t& AFRH = memory(base + 0x24);
-
-	void set_pin_mode(int pin, Mode mode)
-	{
-		write_bits(MODER, 0x3, 2*pin, static_cast<int>(mode));
-	}
-
-	void set_pin_speed(int pin, Speed speed)
-	{
-		write_bits(OSPEEDR, 0x3, 2*pin, static_cast<int>(speed));
-	}
-
-	void set_pin_pull(int pin, Pull pull)
-	{
-		write_bits(PUPDR, 0x3, 2*pin, static_cast<int>(pull));
-	}
-
-	void set_pin_output_type(int pin, OutputType otype)
-	{
-		write_bits(OTYPER, 0x1, pin, static_cast<int>(otype));
-	}
-
-	void set_pin_alternate_function(int pin, int af)
-	{
-		if (pin < 8)
-		{
-			write_bits(AFRL, 0xF, 4*pin, af);
-		}
-		else
-		{
-			pin -= 8;
-			write_bits(AFRH, 0xF, 4*pin, af);
-		}
-	}
-
-	void set_pin(int pin) { BSRR = 1U << pin; }
-
-	void clr_pin(int pin) { BSRR = (1U << 16) << pin; }
-
-	void tgl_pin(int pin) { ODR ^= (1 << pin); }
-
 };
 
 template<Port P, int PinNum>
-struct Pin
+class Pin
 {
+public:
 	static_assert(PinNum < 16, "");
 
 	Pin () {}
@@ -107,28 +71,51 @@ struct Pin
 		set_alternate_function(af);
 	}
 
-	void set_mode(Mode mode) { _port.set_pin_mode(PinNum, mode); }
+	void set_mode(Mode mode)
+	{
+		write_bits(_port.MODER, 0x3, 2*PinNum, static_cast<int>(mode));
+	}
 
-	void set_speed(Speed speed) { _port.set_pin_speed(PinNum, speed); }
+	void set_speed(Speed speed)
+	{
+		write_bits(_port.OSPEEDR, 0x3, 2*PinNum, static_cast<int>(speed));
+	}
 
-	void set_pull(Pull pull) { _port.set_pin_pull(PinNum, pull); }
+	void set_pull(Pull pull)
+	{
+		write_bits(_port.PUPDR, 0x3, 2*PinNum, static_cast<int>(pull));
+	}
 
-	void set_output_type(OutputType otype) { _port.set_pin_output_type(PinNum, otype); }
+	void set_output_type(OutputType otype)
+	{
+		write_bits(_port.OTYPER, 0x1, PinNum, static_cast<int>(otype));
+	}
 
-	void set_alternate_function(int af) { _port.set_pin_alternate_function(PinNum, af); }
+	template<int N = PinNum>
+	typename std::enable_if<(N < 8)>::type set_alternate_function(int af)
+	{
+		write_bits(_port.AFRL, 0xF, 4*N, af);
+	}
 
-	void set() { _port.set_pin(PinNum); }
+	template<int N = PinNum>
+	typename std::enable_if<(N >= 8)>::type set_alternate_function(int af)
+	{
+		write_bits(_port.AFRH, 0xF, 4*(N - 8), af);
+	}
 
-	void clr() { _port.clr_pin(PinNum); }
+	void set() { _port.BSRR = 1U << PinNum; }
 
-	void tgl() { _port.tgl_pin(PinNum); }
+	void clr() { _port.BSRR = (1U << 16) << PinNum; }
+
+	void tgl() { _port.ODR ^= (1 << PinNum); }
 
 private:
 	GPIO<P> _port;
 };
 
-struct WWDG
+class WWDG
 {
+public:
 	static constexpr uintptr_t base = 0x40002C00U;
 	volatile uint32_t& CR = memory(base + 0x00);
 };
@@ -138,9 +125,10 @@ int main()
 	RCC rcc;
 	rcc.enable_all_gpio_clocks();
 
-	Pin<Port::B, 7> led_blue(Mode::OUTPUT, Speed::VERY_HIGH, Pull::NONE, OutputType::PUSH_PULL, 0);
-	Pin<Port::B, 14> led_red(Mode::OUTPUT, Speed::VERY_HIGH, Pull::NONE, OutputType::PUSH_PULL, 0);
+	Pin<Port::B, 7> led_blue(Mode::Output, Speed::VeryHigh, Pull::None, OutputType::PushPull, 0);
+	Pin<Port::B, 14> led_red(Mode::Output, Speed::VeryHigh, Pull::None, OutputType::PushPull, 0);
 
+	led_red.clr();
 	led_blue.set();
 
 	while (1)
