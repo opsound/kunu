a2497dc935073833c3a2029bb729104b294e96be
Alex Mastro <opsound@gmail.com>
2017-03-10 20:17:45 -0800

    Reorder led declaration

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
 	RCC rcc;
 	rcc.enable_all_gpio_clocks();
 
-	Pin<Port::B, 7> led_blue(Mode::Output, Speed::VeryHigh, Pull::None, OutputType::PushPull, 0);
 	Pin<Port::B, 14> led_red(Mode::Output, Speed::VeryHigh, Pull::None, OutputType::PushPull, 0);
+	Pin<Port::B, 7> led_blue(Mode::Output, Speed::VeryHigh, Pull::None, OutputType::PushPull, 0);
 
 	led_red.clr();
 	led_blue.set();
